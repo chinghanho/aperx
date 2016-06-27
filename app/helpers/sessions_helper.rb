fd7eac97ef9c71abfb661b9b2830efb1396b5740
CHH <chh@chh.tw>
2016-06-27 12:57:31 +0800

    建立 sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,47 @@
 module SessionsHelper
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        login user
+        @current_user = user
+      end
+    end
+
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def logout
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def store_location
+    session[:return_to] = request.get? ? request.request_uri : request.referer
+  end
+
 end
