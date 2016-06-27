2a81163ceb3475e6e9c9c459c902f056f26ba3de
CHH <chh@chh.tw>
2016-06-27 12:57:51 +0800

    完成 model user 的內容

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,81 @@
 #
 
 class User < ActiveRecord::Base
+
+  attr_accessor :remember_token, :reset_token
+
+  enum role: [:admin, :user]
+
+  DEFAULT_ROLE = 'user'
+  default_value_for :role, DEFAULT_ROLE
+
+  before_save :downcase_email
+
+
+
+  #
+  # Validations
+  #
+
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, confirmation: true, length: { minimum: 8 }
+
+
+
+  #
+  # Scopes
+  #
+
+  scope :recent, -> { order('id DESC') }
+
+
+
+  #
+  # Instance methods
+  #
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_token, User.digest(remember_token))
+  end
+
+  def forget
+    update_attribute(:remember_token, nil)
+  end
+
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password? token
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  private
+
+    def downcase_email
+      self.email = (email || "").downcase
+    end
+
 end
